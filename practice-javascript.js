13df17471e374e96eb29b5021e095f5dd5ca6c9e
jeffstienstra <jeffstienstra@gmail.com>
2021-09-19 21:56:07 -0400

    initial commit

---
+++ practice-javascript.js
@@ -0,0 +1,31 @@
+// var words = ["quick", "brown", "fox"];
+// words.push("jumps");
+// words.push("over");
+// console.log(words);
+
+// var numbers = [4, 5];
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// var cities = ["Chicago", "New York", "Grand Rapids"];
+// let i = 0;
+// while (i < cities.length) {
+//   if (cities[i].length > 8) {
+//     console.log(cities[i]);
+//   }
+//   i++;
+// }
+
+// var countries = ["Argentina", "Canada", "Guam"];
+// countries.push("USA");
+// console.log(countries);
+
+// 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+
+// var fruits = { watermellon: "red", lemon: "yellow", lime: "green" };
+// fruits["tomato"] = "reddish";
+
+// var toys = { perler_beads: "plastic", he_man: "also plastic", gi_joe: "very plastic" };
+// toys["barbie"] = "completely plastic";
+// console.log(toys);
