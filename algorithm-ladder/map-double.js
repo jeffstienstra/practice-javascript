66bb26b7242ba7035780c6006516fdebec3f40b3
jeffstienstra <jeffstienstra@gmail.com>
2021-09-25 12:21:38 -0400

    add map-double challenge

---
+++ algorithm-ladder/map-double.js
@@ -0,0 +1,16 @@
+/* Given an array of numbers, write a function that returns a new array 
+whose values are the original array’s value doubled.*/
+
+Input: [4, 2, 5, 99, -4];
+Output: [8, 4, 10, 198, -8];
+
+const numbers = [4, 2, 5, 99, -4];
+var doubledNumbers = [];
+
+let doubler = (array) => {
+  array.map((num) => {
+    doubledNumbers.push(num * 2);
+  });
+  return doubledNumbers;
+};
+console.log(doubler(numbers));
